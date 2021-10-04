86d7a92a981813b68b17a8291c73fb665fb196fd
Mark Newman <mwnewman@umich.edu>
2021-10-04 05:22:00 -0400

    updated for f21

--- thiz.js
+++ thiz.js
@@ -1,21 +1,16 @@
 
-// class Person {
+class Person {
 
-//     constructor(nm) {
-//         this.name = nm;
-//     }
+    constructor(nm) {
+        this.name = nm;
+    }
 
-//     greet() {
-//         //console.log("Hi, I'm " + this.name + "!");
-//         console.log("In greet(), this is ", this);
-//     }
+    greet() {
+        console.log("Hi, I'm " + this.name + "!");
+        console.log("In greet(), this is ", this);
+    }
 
-// }
-
-// let joe = new Person("Joe");
-// joe.greet();
-
-// setTimeout(joe.greet, 500);
+}
 
 class Person2 {
 
@@ -25,12 +20,15 @@ class Person2 {
 
     greet = () => {
         console.log("Hi, I'm " + this.name + "!");
-        //console.log("In greet(), this is ", this);
+        console.log("In greet(), this is ", this);
     }
 
 }
 
+let joe = new Person("Joe");
+joe.greet();
+setTimeout(joe.greet, 400);
+
 let jen = new Person2("Jen");
 jen.greet();
-
 setTimeout(jen.greet, 500);
